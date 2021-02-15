743f6ac9887ba644b66c558b86fec45026097962
Taylor Walker <t.walk161@gmail.com>
2021-02-15 17:06:48 +0000

    Added 'createMessage' and 'random' functions to mixedMessages.js

--- mixedMessage.js
+++ mixedMessage.js
@@ -10,3 +10,13 @@ const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
 const weather = ['Snowing', 'Raining', 'Clear', 'Sunny', 'Lightning'];
 const emotions = ['Happy', 'Sad', 'Angry', 'Embarrased', 'Tired'];
 
+//This function will return a string with a set phrase with a random word from each of the above data sets in it
+createMessage = () => {
+    return `Today it is ${days[random(days.length)]}, I am ${emotions[random(emotions.length)]} and it is ${weather[random(weather.length)]} outside.`;
+}
+
+//This function will return a random integer between 0 & including max
+random = max =>{
+    return Math.floor(Math.round()*(max+1));
+}
+
