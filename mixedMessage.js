e7921a9d4be5301ad4ba7135f8aeef15089d4955
Taylor Walker <t.walk161@gmail.com>
2021-02-15 02:18:23 +0000

    Added mixedMessages.js w/ heading and data arrays

---
+++ mixedMessage.js
@@ -0,0 +1,12 @@
+//Mixed Messages by Taylor Walker
+//This project was completed during the CodeCademy Full-Stack Enginerring Course
+//This is the project's main
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+//These are sets of data that I will use to put in a pre-formatted message
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const weather = ['Snowing', 'Raining', 'Clear', 'Sunny', 'Lightning'];
+const emotions = ['Happy', 'Sad', 'Angry', 'Embarrased', 'Tired'];
+
